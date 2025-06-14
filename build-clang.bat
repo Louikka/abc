9edadc513df85180daaab2e28edba77879e5a0b6
Louikka <lolikadimonaa@gmail.com>
2025-06-14 20:55:19 +0300

    added interactivity

--- build-clang.bat
+++ build-clang.bat
@@ -2,17 +2,20 @@
 
 
   :: name of the source file
-set f=clang
+set /p f=Enter name of the source file : 
 
 
   :: compile into assembly
-gcc %f%.c  -S
+:: gcc %f%.c  -S
 
   :: compile into object file
 gcc %f%.c  -c  -Wall -O2
 
   :: linking into executable
 gcc %f%.o  -o %f%.exe  -s
+
+
+echo Compiled successfully!
 
 
 pause
